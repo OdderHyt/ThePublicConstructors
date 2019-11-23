ba537d08fd02d9ef81d0d3fca8423c176cb7c695
Nivlon <fdemet18@student.aau.dk>
2019-11-23 13:13:17 +0100

    Added background and player movement

---
+++ Assets/Scripts/BGController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGController : MonoBehaviour
+{
+    public float speed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(0,transform.position.y+speed);
+        //if(transform.position.y>15)
+            //transform.position = new Vector2(0, -15);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public int playerNumber = 1;
+    public float horizontalSpeed = 10f,
+                 upSpeed = 10f,
+                 downSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerNumber == 1)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+                rb.AddForce(new Vector2(-horizontalSpeed, 0));
+            if (Input.GetKey(KeyCode.RightArrow))
+                rb.AddForce(new Vector2(horizontalSpeed, 0));
+            if (Input.GetKey(KeyCode.UpArrow))
+                rb.AddForce(new Vector2(0, upSpeed));
+            if (Input.GetKey(KeyCode.DownArrow))
+                rb.AddForce(new Vector2(0, -downSpeed));
+        }
+        else if (playerNumber == 2)
+        {
+            if (Input.GetKey(KeyCode.A))
+                rb.AddForce(new Vector2(-horizontalSpeed, 0));
+            if (Input.GetKey(KeyCode.D))
+                rb.AddForce(new Vector2(horizontalSpeed, 0));
+            if (Input.GetKey(KeyCode.W))
+                rb.AddForce(new Vector2(0, upSpeed));
+            if (Input.GetKey(KeyCode.S))
+                rb.AddForce(new Vector2(0, -downSpeed));
+        }
+    }
+}
